8c31505493bf6ea53732273259b25d55a6112eed
huihui <skyalpha@126.com>
2017-05-06 22:43:34 +0800

    add designSystem

---
+++ src/designSystem.js
@@ -0,0 +1,32 @@
+export const colors = {
+  Haus: '#F3F4F4',
+  Night: '#333',
+  Sur: '#96DBE4',
+  'Sur a11y': '#24828F',
+  Peach: '#EFADA0',
+  'Peach a11y': '#E37059',
+  Pear: '#93DAAB',
+  'Pear a11y': '#2E854B',
+};
+
+const typeSizes = [36, 32, 28, 24, 20];
+
+const fontFamilies = {
+  display: 'PingFang SC',
+  body: 'Georgia',
+};
+
+const fontWeights = {
+  regular: 'regular',
+  bold: 'bold',
+};
+
+export const fonts = {
+  'Title 1': {
+    color: colors.Night,
+    fontSize: typeSizes[0],
+    fontFamily: fontFamilies.display,
+    fontWeight: fontWeights.bold,
+    lineHeight: 48,
+  },
+};
